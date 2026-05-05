6db9acfb2c5cf594edff89dbbab17e171a839149
Henmac <henmac1405@gmail.com>
2026-05-05 15:10:21 +0700

    Initial Commit

---
+++ pointofsale/pointofsaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  pointofsaleApp.swift
+//  pointofsale
+//
+//  Created by user on 05/05/26.
+//
+
+import SwiftUI
+
+@main
+struct pointofsaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
